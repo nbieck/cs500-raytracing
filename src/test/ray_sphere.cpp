75217d9466f5d249ad37d93c78581b888aae61d0
Niklas Bieck <niklasjbieck@gmail.com>
2016-01-30 21:34:25 -0800

    Add test cases for ray-sphere

--- src/test/ray_sphere.cpp
+++ src/test/ray_sphere.cpp
@@ -31,3 +31,31 @@ TEST(ray_sphere_intersections, ray_at_sphere_center)
     ASSERT_TRUE(sphere.Intersect(ray, i));
     ASSERT_FLOAT_EQ(1, i.t);
 }
+
+TEST(ray_sphere_intersections, ray_tangent_to_sphere)
+{
+    Ray ray(Vector3(-2,1,0), Vector3(1,0,0));
+    Sphere sphere(Vector3(0,0,0), 1);
+    Intersection i;
+
+    ASSERT_TRUE(sphere.Intersect(ray, i));
+    ASSERT_FLOAT_EQ(2, i.t);
+}
+
+TEST(ray_sphere_intersections, ray_tangent_to_sphere_behind)
+{
+    Ray ray(Vector3(1, 0, 6), Vector3(0,0,1));
+    Sphere sphere(Vector3(0,0,0), 1);
+    Intersection i;
+    
+    ASSERT_FALSE(sphere.Intersect(ray, i));
+}
+
+TEST(ray_sphere_intersections, ray_mising_sphere)
+{
+    Ray ray(Vector3(0,0,0), Vector3(1,0,1));
+    Sphere sphere(Vector3(0,3,1), 2);
+    Intersection i;
+    
+    ASSERT_FALSE(sphere.Intersect(ray, i));
+}
