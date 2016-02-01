c9975bb3425224d2a750a040ce8e69f300fe004a
Niklas Bieck <niklasjbieck@gmail.com>
2016-01-31 20:21:13 -0800

    Define cylinder test cases

--- src/test/ray_cylinder.cpp
+++ src/test/ray_cylinder.cpp
@@ -2,4 +2,58 @@
 
 #include "../ray_shapes.h"
 
+TEST(ray_cylinder, ray_away_from_cylinder_parallel_to_caps)
+{
+}
 
+TEST(ray_cylinder, ray_towards_cylinder_parallel_to_caps)
+{
+}
+
+TEST(ray_cylinder, ray_inside_cylinder_parallel_to_caps)
+{
+}
+
+TEST(ray_cylinder, ray_tangent_cylinder_parallel_to_caps)
+{
+}
+
+TEST(ray_cylinder, ray_past_cylinder)
+{
+}
+
+TEST(ray_cylinder, ray_away_from_cylinder_caps)
+{
+}
+
+TEST(ray_cylinder, ray_towards_cylinder_caps)
+{
+}
+
+TEST(ray_cylinder, ray_inside_cylinder_parallel_to_axis)
+{
+}
+
+TEST(ray_cylinder, ray_outside_cylinder_parallel_to_axis)
+{
+}
+
+TEST(ray_cylinder, ray_on_cylinder_side_pointing_out)
+{
+}
+
+TEST(ray_cylinder, ray_on_cylinder_side_pointing_in)
+{
+}
+
+TEST(ray_cylinder, ray_on_cylinder_cap_pointing_out)
+{
+}
+
+TEST(ray_cylinder, ray_on_cylinder_cap_pointing_in)
+{
+}
+
+TEST(ray_cylinder, ray_touching_cylinder_edge)
+{
+}
